ebfcb5769e09e8ad2224c6594f2634790f2c97aa
iYamWhatIYam <chuckcartoons@gmail.com>
2019-02-04 11:23:29 -0500

    fixed horizontal movement

--- main.py
+++ main.py
@@ -16,11 +16,6 @@ def runGame():
 
     #YES
     while True:
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
         #window title
         pygame.display.set_caption("Space Invaders Ripoff")
 
--- settings.py
+++ settings.py
@@ -9,4 +9,4 @@ class Settings():
         self.bgColor = (47, 47, 47)
 
         #ship shit
-        self.ship_speed_factor = 1.5
+        self.ship_speed_factor = 0.75
